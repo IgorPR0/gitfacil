91a58e593fbc514b4dd6b6eea55a35ba65592dad
IgorPR0 <igortecnologia2020@gmail.com>
2020-09-30 11:23:43 -0300

    feito jeito mais facil

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitfacil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Novo simples e facil mandar git");
+        }
+    }
+}
